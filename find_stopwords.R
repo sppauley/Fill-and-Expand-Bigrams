6bc67f658dcdfefccbb90631889782f82c3ec7e2
sppauley <seanpatrickpauley@gmail.com>
2023-12-07 15:45:10 -0600

    fixing directory structure

--- find_stopwords.R
+++ find_stopwords.R
@@ -9,3 +9,6 @@ library(readxl)
 # filepath/filename containing bigrams
 # inferred structure: bigram column, issue column
 bigrams <- readxl("bigrams.xlsx")
+
+# change if using different text columns for one of the datasets
+text_column <- "Mention Content"
