c9cdd15253392cada1db4011e511893268086d8b
sppauley <seanpatrickpauley@gmail.com>
2023-12-12 18:58:50 -0600

    filter draft

--- filtering_rules.py
+++ filtering_rules.py
@@ -14,7 +14,7 @@ Returns true if passes check, else false
 def check_in_keywords(root_bigram, keyword_list):
     return root_bigram in keyword_list
 
-# Rule 3 (also includes 2 by logic): checks if the bigram frequency is greater than or equal to 5
+# Rule 2 & 3: checks if the bigram frequency is greater than or equal to 5
 '''
 Takes an argument for output dataframe
 Returns true if passes check, else false
@@ -22,15 +22,15 @@ Returns true if passes check, else false
 def check_frequency(dataframe, frequency = 5):
     return dataframe.shape[0] >= frequency
 
-# Rule 5a_: checks if the bigram with stopwords between can be captured by pre-existing keywords
+# Rule 5 & 6: checks if the bigram with stopwords between can be captured by pre-existing keywords
 '''
 Takes an argument for the stopwords output and the full bigram .xlsx file
 Returns filtered dataframe
 '''
-def filter_capture(stopwords_dataframe, keyword_list, frequency = 5):
+def filter_capture(narrowed_dataframe, phrase_column, keyword_list, frequency = 5):
     
     # filters dataframe based on frequency
-    test_df = stopwords_dataframe[stopwords_dataframe['count'] >= frequency]
+    test_df = narrowed_dataframe[narrowed_dataframe['count'] >= frequency]
 
     '''
     Underlying function for use in lambdas
@@ -41,8 +41,17 @@ def filter_capture(stopwords_dataframe, keyword_list, frequency = 5):
         return any([True if keyword in phrase else False for keyword in keyword_list])
 
     # applies and filters based on the capture check
-    test_df['filter'] = test_df['stopwords_between'].apply(lambda x: check_capture(x))
+    test_df['filter'] = test_df[phrase_column].apply(lambda x: check_capture(x))
+    test_df = test_df.rename(columns={phrase_column:'keyword'})
     return test_df[test_df['filter']==False].drop(columns=['filter'])
+
+# Rule 6: checks if the bigram with words before and/or after can be captured by pre-existing keywords
+'''
+Takes an argument for the stopwords output and the full bigram .xlsx file
+Returns filtered dataframe
+'''
+def filter_order(test_dataframe, keyword_list, frequency = 5):
+    return test_dataframe[test_dataframe['count'] >= frequency]
 
 #=======================================================
 #
@@ -54,19 +63,25 @@ def filter_capture(stopwords_dataframe, keyword_list, frequency = 5):
 Uses a broader structure to combine the rules
 Returns TRUE if the bigram should be included, else FALSE
 '''
-def combined_filter(dataframe, stopwords_dataframe, root_bigram, keyword_list):
+def combined_filter(dataframe, stopwords_dataframe, extension_dataframe, root_bigram, keyword_list): 
 
-    # creates the blank list
-    output = []
+    # creates the blank dataframe
+    output = pd.DataFrame({'keyword':[], 'count':[], 'type':[]})
 
     # include the base bigram IF the first fails and the second two pass
     if not check_in_keywords(root_bigram, keyword_list) and check_frequency(dataframe):
-        output.append(root_bigram)
+        output = pd.concat([output, pd.DataFrame({'keyword':[root_bigram], 'count':[dataframe.shape[0]], 'type':['root bigram']})])
 
-    
+    # filters the stopwords
+    stopwords_filtered = filter_capture(stopwords_dataframe, 'stopwords', keyword_list)
+    if stopwords_filtered.shape[0] > 0:
+        stopwords_filtered['type'] = 'stopwords filled'
+        output = pd.concat([output, stopwords_filtered])
+
+    # filters the extended phrases
+    extensions_filtered = filter_capture(extension_dataframe, 'ngrams', keyword_list)
+    if extensions_filtered.shape[0] > 0:
+        extensions_filtered['type'] = 'extended ngram'
+        output = pd.concat([output, extensions_filtered])
 
     return output
-
-keyword_list = pd.DataFrame({'bigram':['one_two','three_four', 'one_three']})
-df = pd.DataFrame({'stopwords_between':['one two', 'one nine two'], 'count':[6,7]})
-print(filter_capture(df, keyword_list))
--- find_stopwords.py
+++ find_stopwords.py
@@ -18,8 +18,12 @@ text_column = 'Mention Content'
 
 # loads a shared set of stopwords from NLTK and spaCy
 # kept in an external .txt file, feel free to modify with others
-stopwords = open('stopwords 2.txt', 'r', encoding="utf-8").read()
+stopwords = open('stopwords.txt', 'r', encoding="utf-8").read()
 stopwords = stopwords.split('\n')
+
+# loads a set of prior keywords for comparison (to avoid duplicates)
+keyword_list = open('keywords.txt', 'r', encoding="utf-8").read()
+keyword_list = keyword_list.split(', ')
 
 # uses regex and the list of stopwords to create a large regex pattern specifically for stopwords
 # allows for up to quandrigrams
@@ -48,36 +52,25 @@ def find_ngrams(text_series, bigram):
 
         # finds all ngrams starting with the first bigram token and ending with the last
         # adds \w characters, since word PPR removes plurals 's' and verb endings like 'ed'
-        ngrams = re.findall('(\w+\s)?('+bigram_tokens[0]+'\w*'+' '+pattern+bigram_tokens[1]+')\w*'+'(\s\w+)?', text)
+        ngrams = re.findall('((?:\w+\s)?('+bigram_tokens[0]+'\w*'+' '+pattern+bigram_tokens[1]+')\w*'+'(?:\s\w+)?)', text)
         
         return ngrams
 
+    # gets the full list, subsets to stopwords between and then full ngrams
     full_list = [get_ngrams(text.lower()) for text in text_series if type(text)==str]
-
-    words_before = [[entry[0] for entry in ngram_list] for ngram_list in full_list if type(ngram_list)==list]
     stopwords_between = [[entry[1] for entry in ngram_list] for ngram_list in full_list if type(ngram_list)==list]
-    words_after = [[entry[2] for entry in ngram_list] for ngram_list in full_list if type(ngram_list)==list]
+    ngrams_extended = [[entry[0] for entry in ngram_list] for ngram_list in full_list if type(ngram_list)==list]
 
     # flattens the matrices
-    words_before = sum(words_before, [])
+    ngrams_extended = sum(ngrams_extended, [])
     stopwords_between = sum(stopwords_between, [])
-    words_after = sum(words_after, [])
 
-    # creates a set of counts for each, returns them in order as a string
-    output = pd.DataFrame({'words before':words_before, 'words after':words_after, 'stopwords between':stopwords_between})
-    output_stopwords = output.groupby('stopwords between').size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False).head(20)
-    output_before = output.groupby('words before').size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False).head(10)
-    output_after = output.groupby('words after').size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False).head(10)
+    # creates a set of counts for each, returns them in order
+    output = pd.DataFrame({'stopwords':stopwords_between, 'ngrams':ngrams_extended})
+    ngrams_sorted = output.groupby('ngrams').size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False).head(20)
+    stopwords_sorted = output.groupby('stopwords').size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False).head(20)
 
-    # formats as a string
-    if output.shape[0]>0:
-        total = 'total occurrences: '+str(output.shape[0])
-        output_stopwords = re.sub('\n\d+', '\n', str(output_stopwords))
-        output_before = re.sub('\n\d+', '\n', str(output_before))
-        output_after = re.sub('\n\d+', '\n', str(output_after))
-        output = bigram+'\n'+total + '\n\n'+ output_stopwords + '\n\n' + output_before + '\n\n' + output_after
-    else:
-        output = 'No occurrences for '+bigram
+    output = filtering_rules.combined_filter(output, stopwords_sorted, ngrams_sorted, bigram.replace('_',' '), keyword_list)
 
     return output
 
@@ -97,16 +90,16 @@ def fill_bigrams(text_filepath):
     # subsets bigrams to that issue category
     subset = bigrams[bigrams['issue_category']==issue_category]['bigram']
 
-    # sets a formatted blank output string
-    output = ('='*50) + '\n\nBigrams for file: ' + text_filepath + '\n\n'
-    print(output)
+    # creates an empty dataframe for output
+    print(('='*50) + '\n\nBigrams for file: ' + text_filepath + '\n\n')
+    output = pd.DataFrame()
     
     # iterates through the bigrams, finds ngrams for each, concats to output
     for bigram in subset:
         if len(bigram.split('_'))>1:
-            ngrams = find_ngrams(df[text_column], bigram)
-            print(ngrams+'\n\n')
-            output = output + ngrams + '\n\n'
+            bigram_set = find_ngrams(df[text_column], bigram)
+            print(bigram_set)
+            output = pd.concat([output, bigram_set])
 
     return output
 
@@ -119,14 +112,13 @@ def fill_bigrams(text_filepath):
 # gets a list of files in the data folder for subsequent iteration
 file_list = ['data/'+f for f in os.listdir('data') if os.path.isfile('data/'+f)]
 
-# sets a default text string for later output into a file
-text_output = ''
+# sets an empty dataframe for output
+output_df = pd.DataFrame()
 
 # iterates through the files and fills bigrams
 for file in file_list:
-    text_output = text_output + fill_bigrams(file)
+    output_df = pd.concat([output_df, fill_bigrams(file)])
 
-# writes the output to a .txt file
-with open("Output.txt", "w") as text_file:
-    text_file.write(text_output)
+# writes the output to a .xlsx file
+output_df.to_excel('Output.xlsx')
 
